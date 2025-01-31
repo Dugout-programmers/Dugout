ca0f46e460608e525608731f12bbfc5cc609e853
ddongguri-bing <ckdalssnsk@naver.com>
2025-01-31 17:39:58 +0900

    [REFACTOR] 퀴즈 및 토너먼트 주제 미선택시 모달 추가

--- src/pages/TournamentSelect.vue
+++ src/pages/TournamentSelect.vue
@@ -9,6 +9,8 @@ import HandsomePlayerGreen from "@/assets/images/handsome_card_green.svg";
 import PlayBtn from "@/assets/images/play_btn.svg";
 import { useRouter } from "vue-router";
 import { ref } from "vue";
+import { useModalStore } from "@/stores/useModalStore";
+import Modal from "@/components/common/Modal.vue";
 
 const router = useRouter();
 const selectedTournament = ref(null);
@@ -18,30 +20,33 @@ const selectTournament = (tournament) => {
   selectedTournament.value = tournament;
 };
 
+const modalStore = useModalStore();
 // Start 버튼 핸들러
 const startTournament = () => {
   if (selectedTournament.value) {
     router.push(`/game/tournamentselect/${selectedTournament.value}`);
   } else {
-    alert("토너먼트를 선택해주세요!");
+    modalStore.openModal({
+      message: "토너먼트를 선택해주세요!",
+      type: "oneBtn",
+      onConfirm: useModalStore.closeModal,
+    });
   }
 };
 </script>
 <template>
+  <Modal />
   <div class="relative w-max mx-auto">
     <img :src="Monitor" class="h-[670px]" />
     <div
-      class="absolute top-[109px] left-1/2 translate-x-[-49%] w-full px-[89px] flex justify-between"
-    >
+      class="absolute top-[109px] left-1/2 translate-x-[-49%] w-full px-[89px] flex justify-between">
       <button
         @click="selectTournament('traitor')"
         :class="selectedTournament === 'traitor' ? 'text-gameGreen' : ''"
-        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer"
-      >
+        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer">
         <img
           :src="selectedTournament === 'traitor' ? BadPlayerGreen : BadPlayer"
-          class="h-[217px] flex-1 object-contain"
-        />
+          class="h-[217px] flex-1 object-contain" />
         <span
           :class="
             selectedTournament === 'traitor' ? 'text-gameGreen' : 'text-white01'
@@ -54,12 +59,10 @@ const startTournament = () => {
       <button
         @click="selectTournament('hero')"
         :class="selectedTournament === 'hero' ? 'text-gameGreen' : ''"
-        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer"
-      >
+        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer">
         <img
           :src="selectedTournament === 'hero' ? GoodPlayerGreen : GoodPlayer"
-          class="h-[217px] flex-1 object-contain"
-        />
+          class="h-[217px] flex-1 object-contain" />
         <span
           :class="
             selectedTournament === 'hero' ? 'text-gameGreen' : 'text-white01'
@@ -72,16 +75,14 @@ const startTournament = () => {
       <button
         @click="selectTournament('handsome')"
         :class="selectedTournament === 'handsome' ? 'text-gameGreen' : ''"
-        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer"
-      >
+        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer">
         <img
           :src="
             selectedTournament === 'handsome'
               ? HandsomePlayerGreen
               : HandsomePlayer
           "
-          class="h-[217px] flex-1 object-contain"
-        />
+          class="h-[217px] flex-1 object-contain" />
         <span
           :class="
             selectedTournament === 'handsome'
@@ -96,8 +97,7 @@ const startTournament = () => {
     </div>
     <button
       @click="startTournament"
-      class="absolute bottom-[140px] left-1/2 translate-x-[-45%] cursor-pointer"
-    >
+      class="absolute bottom-[140px] left-1/2 translate-x-[-45%] cursor-pointer">
       <img :src="PlayBtn" />
     </button>
   </div>
